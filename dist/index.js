2b771c7fe0ef6bebe492fa52a324d1056ad5c310
ianmurfinxyz <ianmurfin@protonmail.com>
2022-08-29 16:00:03 +0100

    recompiled project

--- dist/index.js
+++ dist/index.js
@@ -9681,10 +9681,10 @@ async function run() {
 	try {
 		const data = await fs.readFile(path, { encoding: 'utf8' });
 		const info = JSON.parse(data);
-		core.setOutput("recipe_id", info.installed.recipe.id);
-		core.setOutput("package_id", info.installed.packages[0].id);
-		core.setOutput("root_path", info.installed.packages[0].cpp_info.rootpath);
-		core.setOutput("version", info.installed.packages[0].cpp_info.version);
+		core.setOutput("recipe_id", info.installed[0].recipe.id);
+		core.setOutput("package_id", info.installed[0].packages[0].id);
+		core.setOutput("root_path", info.installed[0].packages[0].cpp_info.rootpath);
+		core.setOutput("version", info.installed[0].packages[0].cpp_info.version);
 	} 
 	catch(error) {
 		core.setFailed(error.message);
